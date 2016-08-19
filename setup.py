94ff29e262690b75ef23132cdbd1e1d7431a50bd
Antoine Catton <devel@antoine.catton.fr>
2016-08-19 22:58:07 +0200

    Release 0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="ohneio",
-      version="0.8-dev",
+      version="0.8",
       description="Utility to write network protocol parser without any I/O",
       long_description=read('README'),
       license='ISC',
