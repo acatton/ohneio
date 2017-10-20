c7f0b9a7b469409c2146f8c1eb73f0795db727cb
Antoine Catton <devel@antoine.catton.fr>
2017-10-20 23:24:55 +0200

    Add utility functions

--- src/ohneio/utils.py
+++ src/ohneio/utils.py
@@ -1,3 +1,7 @@
+import struct
+import io
+from typing import Any, Callable
+
 from . import Protocol, read, send, putback
 
 __all__ = ['readstr', 'sendstr', 'read_until']
@@ -58,3 +62,45 @@ def read_until(b: bytes, included: bool=True) -> Protocol[bytes]:
     else:
         yield from putback(sep + leftover)
         return data
+
+
+def read_nbytes(n: int) -> Protocol[bytes]:
+    """Read a certain an *exact* amount of bytes.
+
+    Args:
+        n (int): Amount of bytes to be read.
+
+    Returns:
+        bytes that we read.
+    """
+    buf = io.BytesIO()
+    while buf.tell() < n:
+        incomming = yield from read()
+        buf.write(incomming)
+    buf.seek(0, io.SEEK_SET)
+
+    data = buf.read(n)
+    leftover = buf.read()
+    yield from putback(leftover)
+    return data
+
+
+def struct_reader(fmt: str) -> Callable[[], Protocol[Any]]:
+    """Factory for a protocol function that reads a C struct.
+
+    This factory uses the struct.pack() and struct.unpack() in the background. Refers to the module
+    documentation for more information.
+
+    Args:
+        fmt (str): Format string to read the C struct from.
+
+    Returns:
+        A protocol generator.
+    """
+    data_length = struct.calcsize(fmt)
+
+    def generator() -> Protocol[Any]:
+        data = yield from read_nbytes(data_length)
+        return struct.unpack(fmt, data)
+
+    return generator
